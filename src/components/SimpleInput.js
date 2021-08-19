8284ed07261899a568319e4c0d6b2fd268a8149a
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-19 15:50:05 +0200

    form validation and state mgmt with custom hook

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,78 @@
+import useInput from '../hooks/use-input';
+
 const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes('@') && value.trim().length > 3);
+
+  let formIsValid = false;
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  } else {
+    formIsValid = false;
+  }
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
+      return;
+    }
+    resetNameInput();
+    resetEmailInput();
+    console.log(enteredName, enteredEmail);
+  };
+
+  const nameInputClasses = `form-control${nameInputHasError ? ' invalid' : ''}`;
+  const emailInputClasses = `form-control${
+    emailInputHasError ? 'invalid' : ''
+  }`;
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          // ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          value={enteredName}
+        />
+        {nameInputHasError && (
+          <p className='error-text'>Name field cannot be left empty!</p>
+        )}
       </div>
-      <div className="form-actions">
-        <button>Submit</button>
+      <div className={emailInputClasses}>
+        <label htmlFor='email'>Your Email</label>
+        <input
+          type='email'
+          id='email'
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputHasError && (
+          <p className='error-text'>You must enter a valid email!</p>
+        )}
+      </div>
+      <div className='form-actions'>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
